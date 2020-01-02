469d8f88042882e7fcb65a9f72c1d048432abed4
Markus Masalin <markus.masalin@eduvantaa.fi>
2020-01-02 17:51:10 +0200

    uibuild

---
+++ build/precache-manifest.bd0e7caa42ba26baabdd1aa3e7051af1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911835ad081783f7eac1a5dfce4d4362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80449667aaeaebd9d748",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7c088f95c0e770356065",
+    "url": "/static/js/2.f6b0d2d4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.f6b0d2d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80449667aaeaebd9d748",
+    "url": "/static/js/main.3a1c1d43.chunk.js"
+  },
+  {
+    "revision": "002682b26539d68c0472",
+    "url": "/static/js/runtime-main.33fbdb1a.js"
+  }
+]);
